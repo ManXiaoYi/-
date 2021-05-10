21aeb2d719b04d408d88d8f8f17bed1b67e3d433
满孝意 <manxiaoyi@huizhaofang.com>
2021-05-10 20:10:53 +0800

    first commit

---
+++ pages/login/login.js
@@ -0,0 +1,23 @@
+// pages/login/login.js
+Page({
+  wechatLogin: function () {
+    console.log('wechatLogin')
+  },
+
+  doubanLogin: function () {
+    console.log('doubanLogin')
+  },
+
+  openAgreement: function () {
+    wx.navigateTo({
+      url: '/pages/agreement/agreement',
+      success: (result) => {
+
+      },
+      fail: () => { },
+      complete: () => { }
+    });
+
+  }
+
+})
